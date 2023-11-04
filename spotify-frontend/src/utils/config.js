daf4f1ae3d6516d58b3024825c70be592ea9223c
sachinchavda18 <120299726+sachinchavda18@users.noreply.github.com>
2023-11-04 19:49:36 +0530

    backend url updated

--- spotify-frontend/src/utils/config.js
+++ spotify-frontend/src/utils/config.js
@@ -1,1 +1,2 @@
-export const backendUrl =  "http://localhost:8080" || "https://backend-spotify-7wu5.onrender.com" ;
+export const backendUrl =   "https://backend-spotify-7wu5.onrender.com" ;
+// export const backendUrl =  "http://localhost:8080" 
